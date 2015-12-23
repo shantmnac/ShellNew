45cc5d7df051ac0e786b8f45cbd7ea7cdd769f1a
Goalt <yurikonkov@macbook-pro-yuri.local>
2015-12-23 19:12:31 +0300

    Initial Commit

---
+++ Shell_2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Shell_2
+//
+//  Created by Yuri Konkov on 23.12.15.
+//  Copyright © 2015 yuri. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
